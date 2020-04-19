e0baddff6e982a260e757ee38395cac3037e9f80
Greg Hayes <gthelight@gmail.com>
2020-04-19 18:51:32 -0500

    server running, routes commentedout for now

---
+++ actions/action-router.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const actions = require("./data/helpers/actionModel");
+
+const router = express.Router();
---
+++ projects/project-router.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const projects = require("./data/helpers/projectModel");
+
+const router = express.Router();
