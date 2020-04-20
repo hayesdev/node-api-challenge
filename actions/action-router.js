8b666e19403e63e13f25fa96774dd428e195bd23
Greg Hayes <gthelight@gmail.com>
2020-04-19 20:53:40 -0500

    action-router complete, not fully tested yet but no error running server

--- actions/action-router.js
+++ actions/action-router.js
@@ -11,9 +11,9 @@ const router = express.Router();
 // });
 
 // GET action by id
-router.get("/:projectId", (req, res) => {
+router.get("/:actionId", (req, res) => {
   actions
-    .get(req.params.projectId)
+    .get(req.params.actionId)
     .then((id) => {
       if (id) {
         res.status(200).json(id);
@@ -27,6 +27,82 @@ router.get("/:projectId", (req, res) => {
       console.logg(err);
       res.status(500).json({
         error: "There was an error retrieving that project's actions.",
+      });
+    });
+});
+
+// POST add new action
+router.post("/:actionId", (req, res) => {
+  if (
+    !req.params.id ||
+    !req.body.description ||
+    req.body.description.length > 128
+  ) {
+    return res.status(400).json({
+      message: "Please provide a valid description for this action.",
+    });
+  }
+  actions
+    .insert(req.body)
+    .then((project) => {
+      if (project) {
+        res.status(201).json({
+          message: "Action created!",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: "There was an error creating the action.",
+      });
+    });
+});
+
+// PUT edit project
+router.put("/:actionId", (req, res) => {
+  if (!req.params.actionId || !req.body) {
+    return res.status(404).json({
+      message: "That action does not exist",
+    });
+  }
+  actions
+    .update(req.params.actionId, req.body)
+    .then((changes) => {
+      if (changes) {
+        res.status(201).json({
+          message: "Action updated!",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: "Could not update that action.",
+      });
+    });
+});
+
+// DELETE
+router.delete("/:actionId", (req, res) => {
+  //   if (!req.body) {
+  //     res.status(404).json({
+  //       message: "That project does not exist",
+  //     });
+  //   }
+  actions
+    .remove(req.params.actionId)
+    .then((action) => {
+      if (action > 0) {
+        res.status(200).json({
+          message: "Action has been removed.",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: "Error removing the action.",
       });
     });
 });
