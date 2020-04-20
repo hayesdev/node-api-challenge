637d2f6c2aba635ae092d26e8fa47e155c2e9432
Greg Hayes <gthelight@gmail.com>
2020-04-19 20:39:57 -0500

    project-router done

--- actions/action-router.js
+++ actions/action-router.js
@@ -4,11 +4,11 @@ const actions = require("../data/helpers/actionModel");
 const router = express.Router();
 
 // "/" = "/projects/:id/actions"
-router.get("/", (req, res) => {
-  res.json({
-    message: "This is the action router",
-  });
-});
+// router.get("/", (req, res) => {
+//   res.json({
+//     message: "This is the action router",
+//   });
+// });
 
 // GET action by id
 router.get("/:projectId", (req, res) => {
--- projects/project-router.js
+++ projects/project-router.js
@@ -3,13 +3,6 @@ const projects = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
-// "/" = "/projects"
-// router.get("/", (req, res) => {
-//   res.json({
-//     message: "This is the project router",
-//   });
-// });
-
 // GET get all projects
 // router.get("/", (req, res)=> {
 //     projects
@@ -33,7 +26,7 @@ router.get("/:id", (req, res) => {
       if (id) {
         res.status(200).json(id);
       } else {
-        res.status(404).json({
+        return res.status(404).json({
           message: "That project ID does not exist.",
         });
       }
@@ -46,10 +39,31 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// GET finding project actions
+router.get("/:id/actions", (req, res) => {
+  projects
+    .getProjectActions(req.params.id)
+    .then((project) => {
+      if (project.length) {
+        return res.status(400).json({
+          message: "This project has no actions.",
+        });
+      } else {
+        res.status(200).json(project);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: "There was an error retrieving this project's actions.",
+      });
+    });
+});
+
 // POST new project
 router.post("/", (req, res) => {
   if (!req.body.name || !req.body.description) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "Please provide a name and description for this project.",
     });
   }
@@ -73,7 +87,7 @@ router.post("/", (req, res) => {
 // PUT edit project
 router.put("/:id", (req, res) => {
   if (!req.params.id || !req.body) {
-    res.status(404).json({
+    return res.status(404).json({
       message: "That project ID does not exist",
     });
   }
@@ -96,7 +110,7 @@ router.put("/:id", (req, res) => {
 
 // DELETE
 router.delete("/:id", (req, res) => {
-  //   if (!req.params.id) {
+  //   if (!req.body) {
   //     res.status(404).json({
   //       message: "That project does not exist",
   //     });
