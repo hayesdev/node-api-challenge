e7b6911707a5bc3c1c4e18ff56595bb00c2f3b16
Greg Hayes <gthelight@gmail.com>
2020-04-19 19:15:48 -0500

    projects GET by id router handler working

--- actions/action-router.js
+++ actions/action-router.js
@@ -3,6 +3,7 @@ const actions = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
+// "/" = "/projects/:id/actions"
 router.get("/", (req, res) => {
   res.json({
     message: "This is the action router",
--- projects/project-router.js
+++ projects/project-router.js
@@ -3,10 +3,31 @@ const projects = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
+// "/" = "/projects"
 router.get("/", (req, res) => {
   res.json({
     message: "This is the project router",
   });
+});
+
+router.get("/:id", (req, res) => {
+  projects
+    .get(req.params.id)
+    .then((id) => {
+      if (id) {
+        res.status(200).json(id);
+      } else {
+        res.status(404).json({
+          message: "That project ID does not exist",
+        });
+      }
+    })
+    .catch((err) => {
+      console.logg(err);
+      res.status(500).json({
+        error: "There was an error retrieving that project",
+      });
+    });
 });
 
 module.exports = router;
